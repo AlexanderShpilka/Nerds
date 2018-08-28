5cd83124b4ec173d3eebe9f906bd7f7a2b9fa687
AlexanderShpilka <sandro.bot.evil@gmail.com>
2018-08-28 13:15:28 +0300

    Updates

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 0,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: false
+  });
+});
